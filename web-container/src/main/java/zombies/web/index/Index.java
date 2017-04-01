44572ebd98328aebbe72bfe26faf8878abaa32ad
sysone <sysone@sysone-300E5EV-300E4EV-270E5EV-270E4EV-2470EV>
2017-04-01 10:58:06 -0300

    Initial zombies web container commit

---
+++ web-container/src/main/java/zombies/web/index/Index.java
@@ -0,0 +1,25 @@
+package zombies.web.index;
+
+import javax.faces.bean.ManagedBean;
+
+import com.uqbar.vainilla.DesktopGameLauncher;
+
+import zombie.DesktopGameLauncherNoVisible;
+import zombie.SonidoAparte;
+import zombie.Zombies;
+
+@ManagedBean
+public class Index {
+
+	    public void start() {
+			Zombies game = new Zombies();
+			SonidoAparte sonido = new SonidoAparte();
+			new DesktopGameLauncherNoVisible(sonido).launch();
+			
+			Zombies.player = sonido.getSoundPlayer();
+			
+			new DesktopGameLauncher(game).launch();
+	    }
+	 
+	  
+}
